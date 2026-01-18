b862bfd5e0de8135fc89f9b2a06f849790a3380b
itzmebenz13 <109409292+itzmebenz13@users.noreply.github.com>
2026-01-18 21:14:38 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+
+const CACHE_NAME = 'shein-pcheck-v3';
+const ASSETS = [
+    './',
+    './index.html',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
+            
